0022a9b24c6fbb8e324eded694087319574de2b9
Senghoo Kim <shkdmb@gmail.com>
2015-10-05 23:38:10 -0400

    fix env path issue

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,3 @@
-#! /bin/env bash
+#!/usr/bin/env bash
 cd _site
 find . -type f -exec curl --ftp-create-dirs --ftp-ssl -T {} -u $DEPLOY_USERNAME:$DEPLOY_PASSWORD ftp://$DEPLOY_HOST$DEPLOY_REMOTEDIR{} \;
